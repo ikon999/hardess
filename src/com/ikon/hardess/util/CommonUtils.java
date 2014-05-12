fb3ff10537a1b5fa0512313f4dfa18d490f86bd7
ikon999 <262541533@qq.com>
2014-05-12 15:01:23 +0800

    端到端

--- src/com/ikon/hardess/util/CommonUtils.java
+++ src/com/ikon/hardess/util/CommonUtils.java
@@ -1,5 +1,9 @@
 package com.ikon.hardess.util;
-
+/**
+ * 第一个工具类
+ * @author Administrator
+ *
+ */
 public class CommonUtils {
 	public static void main(String[] args) {
 		System.out.println("abc");
